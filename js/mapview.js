8ee39b55bd70b7b8ee5fe1a15037657a6a9bdadb
lydddddddddd <15810271029@163.com>
2018-07-25 23:19:38 +0800

    a

--- js/mapview.js
+++ js/mapview.js
@@ -6,23 +6,36 @@ function load_map_view() {
     var svg = d3.select("#map_view").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var projection = d3.geo.mercator()
+        .scale((width + 1) / 2 / Math.PI)
+        .translate([width / 2, height / 2])
+        .precision(.1);
+      
+    var graticule = d3.geo.graticule();
+
+    svg.append("path")
+        .datum(graticule)
+        .attr("class", "graticule")
+        .attr("d", path);
 
     d3.json("json/world-topo-min.json", function(error, world) {
     	var countries = topojson.feature(world, world.objects.countries).features;
 
     	svg.append("path")
-           .datum(graticule)
-           .attr("class", "choropleth")
-           .attr("d", path);
+            .datum(graticule)
+            .attr("class", "choropleth")
+            .attr("d", path);
       
         var g = svg.append("g");
       
         g.append("path")
-         .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
-         .attr("class", "equator")
-         .attr("d", path);
+            .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
+            .attr("class", "equator")
+            .attr("d", path);
       
         var country = g.selectAll(".country").data(countries);
     });
