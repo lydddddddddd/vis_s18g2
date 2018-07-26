26f638326d16e33baf15b9ffe07cab70547bb82f
SHIYUENING <455611344@qq.com>
2018-07-27 03:45:35 +0800

    a

--- js/treeview.js
+++ js/treeview.js
@@ -5,7 +5,7 @@ var sunburst_treeview = {
         var margin = {top: 10, right: 10, bottom: 10, left: 10},
             width = sunburstWidth - margin.left - margin.right,
             height = sunburstWidth - margin.top - margin.bottom;
-        var  radius = Math.min(width, height) / 2-60,
+        var  radius = Math.min(width, height) / 2-55,
             color = d3.scale.category20c();
         var svg = d3.select("#tree_view").append("svg")
             .attr("width",width)
@@ -21,32 +21,49 @@ var sunburst_treeview = {
             .endAngle(function(d) { return d.x + d.dx; })
             .innerRadius(function(d) { return Math.sqrt(d.y); })
             .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
-
+        var tool_tip = d3.tip()
+                    .attr("class", "d3-tip")
+                    .offset([-8, 0])
+                    .html(function(d) {
+                        return "<span style='color:white'>" + d.properties.name + "</span>";
+                    });
+        svg.call(tool_tip)
         // read data
         d3.json("json/promotion_tree.json",function(error, root){
             if(error) throw error;
-            console.log(root.data[1])
+            console.log(root.data[1].children)
             champion_name = root.data[1].name;
-
+            contest_country = root.data[1].children[0].name+" vs "+ root.data[1].children[1].name;
+            contest_score = root.data[1].children[0].score+" - "+root.data[1].children[1].score;
+           
             var path = svg.datum(root.data[1]).selectAll(".arc_path")
                 .data(partition.nodes)
                 .enter().append("path")
                 .attr("class","arc_path")
                 .attr("d",arc)
                 .style("stroke","#fff")
+                //.attr("display", function(d) { return d.depth ? null : "none"; }) 
                 .style("fill",function(d){
-                    console.log(d.name)
-                    console.log(d.children)
+                    //console.log(d.name)
+                    //console.log(d.children)
+                    //console.log(d.parent)
                     if(d.children || d.name == d.parent.name){
-                        return color(d.name)
+                        if(!d.parent){
+                            return "white"
+                        }
+                        else{
+                            return color(d.name)
+                        }
                     }
                     else {
                         return "lightgrey"
                     }
                     
+                    
                 })
                 .style("fill-rule","evenodd") 
-                .on("mouseover", function(d){
+                .on("mousemove", function(d){
+                    /*
                     console.log(d.name)
                     selectedpath = d3.selectAll(".arc_path")
                     selectedpath.style("opacity",function(dd){
@@ -56,26 +73,52 @@ var sunburst_treeview = {
                         else
                             return 1
                     })
-                    
+                    */
+                    tool_tip.show(d.name)
                 })
                 .on("mouseout", function(d){
 
-                    selectedpath = d3.selectAll(".arc_path")
-                    selectedpath.style("opacity", 1)
-                                
+                    //selectedpath = d3.selectAll(".arc_path")
+                    //selectedpath.style("opacity", 1)
+                    d3.selectAll(".d3-tip").style("opacity", 0)  
                     
+                })
+                .on("click",function(d){
+                    
+                    if(!d.children || d.children.length > 2){
+                        contest_country_text.text("");
+                        contest_score_text.text("");
+                    }
+                    else{
+                        contest_country = d.children[0].name+" vs "+ d.children[1].name;
+                        
+
+                        contest_score = d.children[0].score+" - "+d.children[1].score;
+                        
+                        contest_country_text.text(contest_country)
+                                            
+                        contest_score_text.text(contest_score);
+                    }
                 })
                 .each(stash);
 
             console.log(champion_name)
-            var champion_text = svg.append("text")
-                                    .attr("class","text")
-                                    .attr("x", -margin.left*2-20)
-                                    .attr("y", margin.top)
-                                    .text(champion_name)
-                                    .attr("font-size","20px")
-                                    .attr("fill","white")
-
+            var contest_country_text = svg.append("text")
+                                    .attr("class","contest_text")
+                                    .attr("x", 0)
+                                    .attr("y", -margin.top*1.5)
+                                    .text(contest_country)
+                                    .attr('text-anchor',"middle")
+                                    .attr("font-size","14px")
+                                    .attr("fill","grey")
+            var contest_score_text = svg.append("text")
+                                    .attr("class","contest_text")
+                                    .attr("x", 0)
+                                    .attr("y", margin.top*3)
+                                    .text(contest_score)
+                                    .attr('text-anchor',"middle")
+                                    .attr("font-size","30px")
+                                    .attr("fill","grey")                     
             d3.selectAll("input").on("change",function change(){
                 var value = this.value === "count"? function(){return 1}:function(d){return d.size};
                 path.data(partition.value(value).nodes)
